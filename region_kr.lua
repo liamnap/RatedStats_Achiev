601a6cda147013aa41431f0a262aa595bd900bef
GitHub Action <action@github.com>
2025-08-04 20:01:33 +0000

    Finalize PvP sync for region_kr

--- region_kr.lua
+++ region_kr.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:007b478b0e4e7f1a45bf544f586a23f6f06d78195cd1bdfef508c7aac9e0f38c
-size 2146290
+oid sha256:982190dee08fb5c840b78d6429fb56a166f21c324920fa9539e655e5f4010998
+size 2169021
