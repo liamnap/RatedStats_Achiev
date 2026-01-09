e79dce321083ebd46de28fe33db6c10174691130
liamnap <122205142+liamnap@users.noreply.github.com>
2026-01-09 21:52:28 +0000

    brighter colours

--- achievements.lua
+++ achievements.lua
@@ -143,9 +143,9 @@ local PvpRankColumns = {
     { key = "h5", label = "High Warlord (2400)",   prefix = "High Warlord",   icon = "Interface\\PvPRankBadges\\PvPRank14.blp", hidden = true },
     { key = "h6", label = "Grand Marshal (2400)",  prefix = "Grand Marshal",  icon = "Interface\\PvPRankBadges\\PvPRank14.blp", hidden = true },
     { key = "i",  label = "Elite (2400)",          prefix = "Elite:",         icon = "Interface\\PVPFrame\\Icons\\UI_RankedPvP_06_Small.blp" },
-    { key = "j",  label = "Strategist (2400)",     prefix = "Strategist:",    icon = "Interface\\PVPFrame\\Icons\\UI_RankedPvP_07_Small.blp", tint = { 0.20, 0.61, 0.33 } }, -- brighter green
-    { key = "k",  label = "Glad (2400)",           prefix = "Gladiator:",     icon = "Interface\\PVPFrame\\Icons\\UI_RankedPvP_07_Small.blp", tint = { 0.20, 0.26, 0.78 } }, -- brighter pink
-    { key = "l",  label = "Legend (2400)",         prefix = "Legend:",        icon = "Interface\\PVPFrame\\Icons\\UI_RankedPvP_07_Small.blp", tint = { 0.20, 0.39, 0.18 } }, -- brighter red
+    { key = "j",  label = "Strategist (2400)",     prefix = "Strategist:",    icon = "Interface\\PVPFrame\\Icons\\UI_RankedPvP_07_Small.blp", tint = { 0.20, 1.00, 0.20 } }, -- brighter green
+    { key = "k",  label = "Glad (2400)",           prefix = "Gladiator:",     icon = "Interface\\PVPFrame\\Icons\\UI_RankedPvP_07_Small.blp", tint = { 1.00, 0.35, 0.95 } }, -- brighter pink
+    { key = "l",  label = "Legend (2400)",         prefix = "Legend:",        icon = "Interface\\PVPFrame\\Icons\\UI_RankedPvP_07_Small.blp", tint = { 1.00, 0.35, 0.20 } }, -- brighter red
     { key = "m1", label = "Three's Company (2700)",prefix = "Three's Company",icon = "Interface\\Icons\\Achievement_Arena_3v3_7", hidden = true },
     { key = "n",  label = "Rank 1 (0.1%)",         r1 = true,                 icon = "Interface\\PVPFrame\\Icons\\UI_RankedPvP_07_Small.blp" },
     { key = "o",  label = "Hero (0.5%)",           hero = true,               icons = {
